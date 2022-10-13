854c46c5bf27cd3e1d9a0d51b4f855e3eb495f5c
ChristopherRamosM <chjeramosmo@ittepic.edu.mx>
2022-10-13 12:28:24 -0600

    React PWA

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Principal } from './components/Principal';
+
+const App = () => {
+  return (
+    <div>
+      <h1>React-PWA Equipo 1</h1>
+      { <Principal/> }
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Principal.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Principal = () => {
+    return (
+        <div>
+            <h3>Principal</h3>
+            <button className='btn btn-primary'>Hola</button>
+        </div>
+    )
+}
